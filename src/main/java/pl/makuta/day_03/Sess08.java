2c835f435caaec2646951c67544440b4da475b36
Rafał Makuta <rafalmakuta@o2.pl>
2019-11-02 13:42:48 +0100

    day3 [10]

--- src/main/java/pl/makuta/day_03/Sess08.java
+++ src/main/java/pl/makuta/day_03/Sess08.java
@@ -26,24 +26,23 @@ public class Sess08 extends HttpServlet {
             quiz.put("Ukraina", "Kijów");
             session.setAttribute("quiz", quiz);
         }
-//
-//        Map<String, String> quiz = (Map<String, String>) session.getAttribute("quiz");
-//        int correctAnswer = 0;
-//        for (Map.Entry<String, String> entry : quiz.entrySet()) {
-//            resp.getWriter().println("<form action='' method='get'>");
-//            resp.getWriter().println("Podaj stolicę państwa dla: " + entry.getKey());
-//            resp.getWriter().println("<input type='text' name='country'>");
-////            resp.getWriter().println("<input type='submit' value='wyślij'>");
-//            resp.getWriter().println("</form>");
-//            if(req.getParameter("country").equals(entry.getValue())){
-//                resp.getWriter().println("Dobrze!");
-//                correctAnswer++;
-//            }else {
-//                resp.getWriter().println("Błąd!");
-//            }
-//            resp.getWriter().println("Poprawnych dpowiedzi: " + correctAnswer);
-//        }
-//
+
+        Map<String, String> quiz = (Map<String, String>) session.getAttribute("quiz");
+        List<String> k = new ArrayList<>(quiz.keySet());
+        if(session.getAttribute("correctAnswer") == null){
+            session.setAttribute("correctAnswer", 0);
+        }
+        if(session.getAttribute("count") == null){
+            session.setAttribute("count", 0);
+        }
+
+        resp.getWriter().println("<form action='' method='post'>");
+        resp.getWriter().println("Podaj stolicę państwa dla: " + k.get((int) session.getAttribute("count")));
+        resp.getWriter().println("<input type='hidden' name='country' value=entry.getKey()>");
+        resp.getWriter().println("<input type='hidden' name='correctCapital' value=capital>");
+        resp.getWriter().println("<input type='text' name='capital'>");
+        resp.getWriter().println("<input type='submit' value='wyślij'>");
+        resp.getWriter().println("</form>");
     }
 
     @Override
@@ -53,30 +52,31 @@ public class Sess08 extends HttpServlet {
         resp.setContentType("text/html; charset=UTF-8");
 
         HttpSession session = req.getSession();
-//        if (session.getAttribute("quiz") == null) {
-//            Map<String, String> quiz = new HashMap<>();
-//            quiz.put("Niemcy", "Berlin");
-//            quiz.put("Czechy", "Praga");
-//            quiz.put("Słowacja", "Bratysława");
-//            quiz.put("Ukraina", "Kijów");
-//            session.setAttribute("quiz", quiz);
-//        }
 
         Map<String, String> quiz = (Map<String, String>) session.getAttribute("quiz");
-        int correctAnswer = 0;
-        for (Map.Entry<String, String> entry : quiz.entrySet()) {
-            resp.getWriter().println("<form action='' method='post'>");
-            resp.getWriter().println("Podaj stolicę państwa dla: " + entry.getKey());
-            resp.getWriter().println("<input type='text' name='country'>");
-//            resp.getWriter().println("<input type='submit' value='wyślij'>");
-            resp.getWriter().println("</form>");
-            if(req.getParameter("country").equals(entry.getValue())){
-                resp.getWriter().println("Dobrze!");
-                correctAnswer++;
-            }else {
-                resp.getWriter().println("Błąd!");
-            }
-            resp.getWriter().println("Poprawnych dpowiedzi: " + correctAnswer);
+        List<String> v = new ArrayList<>(quiz.values());
+        List<String> k = new ArrayList<>(quiz.keySet());
+        int x = (int) session.getAttribute("count");
+
+        resp.getWriter().println("<form action='' method='get'>");
+//        resp.getWriter().println(req.getParameter("capital") + " = " + v.get(x));
+        if(req.getParameter("capital").equals(v.get(x))){
+            resp.getWriter().println("Dobrze! " + req.getParameter("capital") + " jest stolicą państwa " + k.get(x) + ".");
+            int correctAnswer = (int) session.getAttribute("correctAnswer");
+            session.setAttribute("correctAnswer", ++correctAnswer);
+        }else {
+            resp.getWriter().println();
+            resp.getWriter().println("Błąd! " + req.getParameter("capital") + " nie jest stolicą państwa " + k.get(x) + ".");
+        }
+        resp.getWriter().println("<input type='submit' value='oK!'>");
+        resp.getWriter().println("</form>");
+
+        session.setAttribute("count", ++x);
+
+        if((int) session.getAttribute("count") == quiz.size()){
+            resp.getWriter().println("Poprawnych odpowiedzi: " + session.getAttribute("correctAnswer"));
+            session.setAttribute("count", 0);
+            session.setAttribute("correctAnswer", 0);
         }
     }
 }
